e5e9fe01aa35ab4ba3ca6cb2699d98e6ee8fbe34
davit <khmergamerdk33@gmail.com>
2022-11-10 16:51:53 +0700

    fix mainactivity

--- app/src/main/java/com/davit/encrypefilesecurity/MainActivity.kt
+++ app/src/main/java/com/davit/encrypefilesecurity/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.davit.encrypefilesecurity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.davit.encrypesecurityfile.EncryptKey
 
 class MainActivity : AppCompatActivity() {
